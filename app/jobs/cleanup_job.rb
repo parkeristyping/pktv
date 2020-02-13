3094ac84527ff39d6decf44c5b2bb0ac9282a21f
Parker Lawrence <parker@alltimepower.com>
2020-02-12 22:31:02 -0600

    cleanup the cleanup command

--- app/jobs/cleanup_job.rb
+++ app/jobs/cleanup_job.rb
@@ -6,6 +6,6 @@ class CleanupJob < ApplicationJob
     Song.where("created_at < now() - interval '5 minutes'").destroy_all
 
     # Find tmp files more than 5 minutes old
-    `find /home/ubuntu/pktv/tmp -mmin +5 -type f -exec rm -fv {} \;`
+    `find /home/ubuntu/pktv/tmp -mmin +5 -type f -exec rm -fv {} +`
   end
 end
